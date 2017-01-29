e9850afd9ce044f0f3e5d85f309f98a3ab64f276
JOHN LEE <nasky987@gmail.com>
2017-01-29 21:02:09 +0900

    init

---
+++ src/main/groovy/readinglist/ReadingListApplication.groovy
@@ -0,0 +1,12 @@
+package readinglist
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ReadingListApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run ReadingListApplication, args
+	}
+}
